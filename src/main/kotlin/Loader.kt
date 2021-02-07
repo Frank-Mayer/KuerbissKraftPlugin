9c8db90ec5aae17b49e47be4112417a292f47c6a
Frank Mayer <frank.mayer19072001@outlook.de>
2021-02-07 12:51:16 +0100

    Renamed command varo -> kuerbisskraft

--- src/main/kotlin/Loader.kt
+++ src/main/kotlin/Loader.kt
@@ -144,7 +144,7 @@ class Loader : JavaPlugin(), Listener, CommandExecutor, KoinComponent {
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (command.name == "varo") {
+        if (command.name == "kürbiss" || command.name == "kurbiss" || command.name == "kuerbiss" || command.name == "kürbis" || command.name == "kurbis" || command.name == "kuerbis") {
             return cmdInterpreter.command(sender, args)
         }
         return false
