2ce7d1997eccfe276d6ce1d13dd4890518bf8995
Frank Mayer <frank.mayer19072001@outlook.de>
2021-02-08 17:49:54 +0100

    Server is default open

--- src/main/kotlin/Settings.kt
+++ src/main/kotlin/Settings.kt
@@ -3,12 +3,13 @@ package main
 object Settings {
     const val oPsGetLog: Boolean = false
     const val storePath = "plugins/KuerbissKraft/"
+
     /**
      * Maximum play time per day in seconds
      */
     const val maxPlayTime: Int = 120
 
-    var open = false
+    var open = true
 
     var quitNotAllowed = false
 }
