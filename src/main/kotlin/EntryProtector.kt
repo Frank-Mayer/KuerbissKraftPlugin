20ce287e5c2ae531b15c1345a06f514ccf90e7b8
Frank Mayer <frank.mayer19072001@outlook.de>
2021-03-14 17:41:53 +0100

    Simplified Entry Protector

--- src/main/kotlin/EntryProtector.kt
+++ src/main/kotlin/EntryProtector.kt
@@ -11,63 +11,29 @@ import org.bukkit.potion.PotionEffectType
 class EntryProtector {
     private val dangerousBlocks = newHashSet(
         Material.LAVA,
-        Material.FIRE
-    )
-    private val enterableBlocks = newHashSet(
-        Material.AIR,
-        Material.CAVE_AIR,
-        Material.VOID_AIR,
-        Material.TORCH,
-        Material.REDSTONE_TORCH,
-        Material.REDSTONE_WALL_TORCH,
-        Material.WALL_TORCH,
-        Material.NETHER_PORTAL,
-        Material.END_PORTAL,
-        Material.WATER,
-        Material.VINE,
-        Material.BLACK_CARPET,
-        Material.BLUE_CARPET,
-        Material.BROWN_CARPET,
-        Material.CYAN_CARPET,
-        Material.GRAY_CARPET,
-        Material.GREEN_CARPET,
-        Material.LIGHT_BLUE_CARPET,
-        Material.LIGHT_GRAY_CARPET,
-        Material.LIME_CARPET,
-        Material.MAGENTA_CARPET,
-        Material.ORANGE_CARPET,
-        Material.PINK_CARPET,
-        Material.PURPLE_CARPET,
-        Material.RED_CARPET,
-        Material.WHITE_CARPET,
-        Material.YELLOW_CARPET,
-        Material.SNOW,
-        Material.LADDER,
-        Material.LARGE_FERN,
-        Material.SEAGRASS,
-        Material.TALL_SEAGRASS
+        Material.FIRE,
     )
 
     fun login(player: Player) {
-        player.addPotionEffect(PotionEffect(PotionEffectType.FIRE_RESISTANCE, 200, 3))
-        player.addPotionEffect(PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 5))
-        player.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 200, 5))
+        player.addPotionEffect(PotionEffect(PotionEffectType.FIRE_RESISTANCE, 200, 16, true, false, false))
+        player.addPotionEffect(PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 32, true, false, false))
+        player.addPotionEffect(PotionEffect(PotionEffectType.SLOW, 200, 200, true, false, false))
     }
 
     fun protect(location: Location) {
         for (y in 0 until 3) {
             val loc = Location(location.world, location.x, location.y + y, location.z)
-            if (!enterableBlocks.contains(loc.block.type)) {
-                Bukkit.broadcastMessage("Removed not enterable Block ${loc.block.type}")
+            if (loc.block.type.isSolid) {
+                Logger.log("Removed not enterable Block ${loc.block.type}")
                 loc.block.type = Material.AIR
             }
         }
-        for (x in -1 until 2) {
+        for (x in -2 until 3) {
             for (y in 0 until 3) {
-                for (z in -1 until 2) {
+                for (z in -2 until 3) {
                     val loc = Location(location.world, location.x + x, location.y + y, location.z + z)
                     if (dangerousBlocks.contains(loc.block.type)) {
-                        Bukkit.broadcastMessage("Removed dangerous Block ${loc.block.type}")
+                        Logger.log("Removed dangerous Block ${loc.block.type}")
                         loc.block.type = Material.AIR
                     }
                 }
